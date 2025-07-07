4748fff6fe381ec8ce5354306446adb0e0f3693c
Bao Fangliang <bobjoy1010@163.com>
2025-07-07 21:10:23 +0800

    更新 subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,9 +5,9 @@ import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
   id: 233,
-  name: 'Subscription',
+  name: 'Bobjoy的GKD订阅',
   version: 0,
-  author: 'author',
+  author: 'Bobjoy',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
