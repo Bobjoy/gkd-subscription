230fcc215b7ff99a1ae9264ecfff3fc53ecf30df
fangliangbao <fangliangbao@upchina.com>
2025-07-08 13:07:55 +0800

    抖音

---
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -0,0 +1,44 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.ss.android.ugc.aweme',
+  name: '抖音',
+  groups: [
+    {
+      name: '领取成功',
+      key: 1,
+      fastQuery: true,
+      resetMatch: 'app',
+      activityIds: ['com.ss.android.excitingvideo.ExcitingVideoActivity'],
+      rules: 'com.lynx.tasm.behavior.ui.view.UIView[text^="领取成功"]',
+    },
+    {
+      name: '领取奖励',
+      key: 2,
+      fastQuery: true,
+      resetMatch: 'app',
+      activityIds: ['com.ss.android.excitingvideo.ExcitingVideoActivity'],
+      rules: 'com.lynx.tasm.behavior.ui.text.FlattenUIText[text="领取奖励"]',
+    },
+    {
+      name: '退出直播',
+      key: 3,
+      fastQuery: true,
+      resetMatch: 'app',
+      activityIds: ['com.ss.android.ugc.aweme.live.LivePlayActivity'],
+      rules:
+        'android.widget.Button[id="com.ss.android.ugc.aweme.lite:id/root"]',
+    },
+    {
+      name: '评价并收下金币',
+      key: 4,
+      fastQuery: true,
+      resetMatch: 'app',
+      activityIds: [
+        'com.ss.android.ugc.aweme.bullet.ui.BulletContainerActivity',
+      ],
+      rules:
+        'com.lynx.tasm.behavior.ui.text.FlattenUIText[text="评价并收下金币"]',
+    },
+  ],
+});
